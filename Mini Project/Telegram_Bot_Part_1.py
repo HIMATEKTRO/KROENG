a5029e62b3970fa43f0089370fe582a3f34b0943
HIMATEKTRO <100829918+HIMATEKTRO@users.noreply.github.com>
2022-03-18 12:03:00 +0700

    Update Telegram_Bot_Part_1.py

--- Mini Project/Telegram_Bot_Part_1.py
+++ Mini Project/Telegram_Bot_Part_1.py
@@ -1,1 +1,20 @@
-tes
+# mengimport package pyTelegramBotAPI
+import telebot
+
+# inisialisasi Token Bot Kita
+bot = telebot.TeleBot('TOKENBOT')
+
+# Menghandle Pesan /start
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    # membalas pesan
+    bot.reply_to(message, 'Halo bro, ada apa?')
+
+# Menghandle Pesan hai
+@bot.message_handler(regexp='hai')
+def welcome(message):
+    # membalas pesan
+    bot.reply_to(message, "what's up!")
+
+print('bot running!!!')
+bot.polling()
